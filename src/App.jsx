0d68dc5b786d2684f3b3ff35f9d5108751a1f877
Soumya Ranjan Harichandan <skharichandan111@gmail.com>
2026-02-20 18:47:54 +0530

    Initial Stage Game Container

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,23 @@
 
 function App() {
-  
+  return <main>
+    <div id="game-container">
+      <ol id="players">
+        <li>
+          <span className="player-name">Player 1</span>
+          <span className="player-symbol">X</span>
+        </li>
+        <li>
+          <span className="player-name">Player 2</span>
+          <span className="player-symbol">0</span>
+        </li>
 
-  return (
-    <p>Coming soon...</p>
-      );
+        
+
+      </ol>
+      GAME BOARD
+    </div>
+  </main>
 }
 
 export default App
