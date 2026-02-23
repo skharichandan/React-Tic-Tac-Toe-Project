4d7827ae8741dca6b49c82cdbdbcdb534e5453df
Soumya Ranjan Harichandan <skharichandan111@gmail.com>
2026-02-23 12:03:46 +0530

    User Input & Two-Way-Binding

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,29 +1,33 @@
-import {useState} from 'react';
+import { useState } from "react";
 
-export default function Player({name,symbol}) {
+export default function Player({ initialName, symbol }) {
+  const [playerName,setPlayerName] = useState(initialName);
+  const [isEditing, setIsEditing] = useState(false);
 
-const [isEditing,setIsEditing] = useState(false);
+  function handleEditClick() {
+    setIsEditing((editing) => !isEditing);
+  }
 
-function handleEditClick() {
-  setIsEditing(editing => !isEditing);
-  // setIsEditing(editing => !editing);
-}
+  function handleChange(event) {
+     console.log(event);
+    setPlayerName(event.target.value);
+  }
 
-let playerName = <span className="player-name">{name}</span>;
+  let editablePlayerName = <span className="player-name">{playerName}</span>;
 
-
-if(isEditing) {
-  playerName = <input type='text' required  value = {name}/>;
-  
-}
-
-    return (
-        <li>
-          <span className="player">
-            {playerName}
-            <span className="player-symbol">{symbol}</span>
-          </span> 
-          <button onClick = {handleEditClick}>{isEditing ? 'Save' : 'Edit'}</button>
-        </li>
+  if (isEditing) {
+    editablePlayerName = (
+      <input type="text" required value={playerName} onChange={handleChange}/>
     );
+  }
+
+  return (
+    <li>
+      <span className="player">
+        {editablePlayerName}
+        <span className="player-symbol">{symbol}</span>
+      </span>
+      <button onClick={handleEditClick}>{isEditing ? "Save" : "Edit"}</button>
+    </li>
+  );
 }
