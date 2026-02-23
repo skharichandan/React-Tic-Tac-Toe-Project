36275b3c352bfcf7a937e3ac35282b1d9ff302bf
Soumya Ranjan Harichandan <skharichandan111@gmail.com>
2026-02-23 12:39:59 +0530

    Initial GameBoard Component and Rendering Multi-Dimensional Lists

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import Player from "./components/Player"
+import GameBoard from "./components/GameBoard"
 
 function App() {
   return <main>
@@ -7,7 +8,7 @@ function App() {
         <Player initialName="Player-1" symbol="X"/>
         <Player initialName="Player-2" symbol="0"/>
       </ol>
-      GAME BOARD
+      <GameBoard />
     </div>
   </main>
 }
