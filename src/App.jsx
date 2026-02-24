b1a1f92a42394388a7dfd7f1d9e87bd6565c7d24
Soumya Ranjan Harichandan <skharichandan111@gmail.com>
2026-02-24 12:46:11 +0530

    Lifting State, Switching bt Players with Symbol

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,27 @@
+import { useState } from "react"
 import Player from "./components/Player"
 import GameBoard from "./components/GameBoard"
 
 function App() {
-  return <main>
-    <div id="game-container">
-      <ol id="players">
-        <Player initialName="Player-1" symbol="X"/>
-        <Player initialName="Player-2" symbol="0"/>
-      </ol>
-      <GameBoard />
-    </div>
-  </main>
+  const[activePlayer,setActivePlayer] = useState('X');
+
+  function handleSelectSquare() {
+    setActivePlayer((curActivePlayer) => curActivePlayer === 'X' ? 'O' : 'X');
+  }
+
+
+
+  return (
+    <main>
+      <div id="game-container">
+        <ol id="players" className="highlight-player">
+          <Player initialName="Player-1" symbol="X" isActive={activePlayer === 'X'}/>
+          <Player initialName="Player-2" symbol="0" isActive={activePlayer === 'O'}/>
+        </ol>
+        <GameBoard onSelectSquare={handleSelectSquare} activePlayerSymbol={activePlayer}/>
+      </div>
+    </main>
+  );
 }
 
 export default App
