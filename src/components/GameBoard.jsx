657cc15b21f29f78daafdeac7f3a4046ec6cd475
Soumya Ranjan Harichandan <skharichandan111@gmail.com>
2026-02-24 17:35:54 +0530

    Disabling Buttons Conditionally

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -6,27 +6,20 @@ const initialGameBoard = [
   [null, null, null],
 ];
 
-export default function GameBoard({onSelectSquare, turns}) {
-
+export default function GameBoard({ onSelectSquare, turns }) {
   let gameBoard = initialGameBoard;
 
   for (const turn of turns) {
-    const {square , player } = turn;
+    const { square, player } = turn;
     const { row, col } = square;
 
     gameBoard[row][col] = player;
   }
 
-
-
-
-
-
-
   // const [gameBoard,setGameBoard] = useState(initialGameBoard);
 
   // function handleSelectSquare(rowIndex,colIndex) {
-    
+
   //   setGameBoard((prevGameBoard) => {
   //     const updatedBoard = [...prevGameBoard.map(innerArray => [...innerArray])];
   //     updatedBoard[rowIndex][colIndex] = activePlayerSymbol;
@@ -36,8 +29,6 @@ export default function GameBoard({onSelectSquare, turns}) {
   //   onSelectSquare();
   // }
 
-
-
   return (
     <ol id="game-board">
       {gameBoard.map((row, rowIndex) => (
@@ -45,7 +36,9 @@ export default function GameBoard({onSelectSquare, turns}) {
           <ol>
             {row.map((playerSymbol, colIndex) => (
               <li key={colIndex}>
-                <button onClick={() => onSelectSquare(rowIndex,colIndex)}>{playerSymbol}</button>
+                <button onClick={() => onSelectSquare(rowIndex, colIndex)} disabled = {playerSymbol != null}>
+                  {playerSymbol}
+                </button>
               </li>
             ))}
           </ol>
